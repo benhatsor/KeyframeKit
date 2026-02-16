28c02238883bbf4386d5ee43c77e52fd4be7a605
Ben Hatsor <34835685+benhatsor@users.noreply.github.com>
2026-02-16 02:33:36 +0200

    Update KeyframeKit.ts

--- KeyframeKit.ts
+++ KeyframeKit.ts
@@ -67,6 +67,7 @@ function parseKeyframesRule({ rule: keyframes }: {
 
     for (const propertyName of keyframe.style) {
 
+      /// https://developer.mozilla.org/en-US/docs/Web/API/CSSStyleDeclaration/getPropertyValue
       const propertyValue = keyframe.style.getPropertyValue(propertyName);
       
       parsedProperties[propertyName] = propertyValue;
