51b436000fd62e1c5aee1e199704b278b9015c92
Ben Hatsor <34835685+benhatsor@users.noreply.github.com>
2026-03-08 04:04:00 +0200

    bump

---
+++ rollup.config.js
@@ -0,0 +1,43 @@
+import typescript from '@rollup/plugin-typescript';
+import terser from '@rollup/plugin-terser';
+
+import MagicString from 'magic-string';
+
+function header() {
+
+	return {
+
+		renderChunk( code ) {
+
+			code = new MagicString( code );
+
+			code.prepend( `/*
+ * KeyframeKit
+ * MIT License
+ */\n` );
+
+			return {
+				code: code.toString(),
+				map: code.generateMap()
+			};
+
+		}
+
+	};
+
+}
+
+/**
+ * @type {import('rollup').RollupOptions}
+ */
+export default {
+  input: {
+    'KeyframeKit.min.js': 'src/KeyframeKit.ts'
+  },
+  output: {
+    dir: 'dist',
+    entryFileNames: '[name]',
+    sourcemap: true
+  },
+  plugins: [typescript(), terser(), header()]
+};
