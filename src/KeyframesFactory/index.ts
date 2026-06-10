1fac6cfd53084d58713425a96142a47a0479649a
Ben Hatsor <34835685+benhatsor@users.noreply.github.com>
2026-06-10 20:10:33 +0300

    style: Explicit error imports

--- src/KeyframesFactory/index.ts
+++ src/KeyframesFactory/index.ts
@@ -3,11 +3,14 @@
  * @group Default Export
  */
 
-export * from './errors';
+export { KeyframesRuleNameTypeError, SourceTypeError } from './errors';
+
 export { getDocumentStyleSheetsOnLoad } from './sourcing-stylesheets/getDocumentStyleSheetsOnLoad';
 export { importStyleSheet } from './sourcing-stylesheets/importStyleSheet';
+
 export { getStyleSheetKeyframes } from './parsing-stylesheet-keyframes/getStyleSheetKeyframes';
 export { getAllStyleSheetKeyframesRules } from './parsing-stylesheet-keyframes/getAllStyleSheetKeyframesRules';
 export { parseKeyframesRule } from './parsing-stylesheet-keyframes/parseKeyframesRule';
+
 export { ParsedKeyframes, type ParsedKeyframesRules } from './data-types/ParsedKeyframes';
 export { type CSSStyleSheetSource } from './data-types/CSSStyleSheetSource'
